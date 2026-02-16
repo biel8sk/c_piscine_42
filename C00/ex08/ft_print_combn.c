51a7012a031c23e27c2c530271cf56b77b436b59
biel8sk <dasaybarreto724@gmail.com>
2026-02-16 00:00:42 -0300

    iniciando algoritmo

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -1,17 +1,66 @@
 
 #include <unistd.h>
 
+int get_max(int len)
+{
+	int i = 1;
+	while (len > 0)
+	{
+		i *= 10;
+	}
+	return (i - 1);
+}
+
+
+int get_power_ten(int i)
+{
+	int k = 1;
+	int count = 1;
+
+	while (k < i)
+	{
+		count ++;
+		k *= 10;
+	}
+	return (count);
+}
+
 void	ft_print_combn(int n)
 {
 	int i;
-	
-	0 = 1;
-	while (i <= 5)
+	int max;
+	int j;
+	int temp;
+	int curr_len;
+
+	i = 0;
+	max = get_max(i);
+	j = 1;
+	temp = 0;
+	curr_len = 1;
+	while (i < max)
 	{
-		n += i; 
-		write(1, &n, 1);
-		n -= i;
-		i = i + 1;
+		if (i < 10)
+		{
+			while (j < n)
+			{
+				write(1, "0", 1);
+				j++;
+			}
+			temp = i + 48;
+			write(1, &temp, 1);
+		}
+		else
+		{
+			curr_len = get_power_ten(i);
+			j = curr_len;
+			while (j < n)
+			{
+				write(1, "0", 1);
+				j++;
+			}
+			
+		}
 	}
 }
 
