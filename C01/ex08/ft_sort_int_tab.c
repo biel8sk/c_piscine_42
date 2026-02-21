4fe695d1da2cdb367e6f91c49f58bc6f16f8908f
biel8sk <dasaybarreto724@gmail.com>
2026-02-20 21:29:29 -0300

    righting ex08

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -5,28 +5,29 @@ void	ft_sort_int_tab(int *tab, int size)
 		int	temp;
 
 		i = 0;
-		while (i < (size -1))
+		j = 0;
+		while (i < size)
 		{
-			j = 0;
 			while (j < (size -1))
 			{
-				if (tab[i] < tab [j])
+				if (tab[j] > tab [j + 1])
 				{
-					temp = tab[i];
-					tab[i] = tab[j];
-					tab[j] = temp;
+					temp = tab[j];
+					tab[j] = tab[j + 1];
+					tab[j + 1] = temp;
 				}
 				j++;
 			}
+			j = 0;
 			i++;
 		}
 }
 
-int	main(void)
+/*int	main(void)
 {
 	#include <stdio.h>
 	int p[6] = {4,6,3,5,1,2};
 	ft_sort_int_tab(p, 6);
 	for (int i = 0; i < 6; i++)
 		printf("%d\n", p[i]);
-}
+}*/
