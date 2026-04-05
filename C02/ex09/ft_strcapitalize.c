a03f82062a50b7a8dd86fb11b2768fb97e89c50b
biel8sk <dasaybarreto724@gmail.com>
2026-04-04 22:56:55 -0300

    fix: strcapitalize

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -18,8 +18,6 @@ int	is_alfa(char c)
 	return (is_upper(c) || is_lower(c) || is_num(c));
 }
 
-#include <stdio.h>
-
 char	*ft_strcapitalize(char *str)
 {
 	int i;
@@ -28,7 +26,6 @@ char	*ft_strcapitalize(char *str)
 	printf("execitando o metodo");
 	while (str[i] != 0)
 	{
-		printf("entrou no loop");
 		if (i == 0 && is_lower(str[i]))
 		{
 
@@ -38,21 +35,20 @@ char	*ft_strcapitalize(char *str)
 		}
 		else 
 		{
-			/*if (is_lower(str[i]) && (!is_alfa(str[i -1])))
+			if (is_lower(str[i]) && (!is_alfa(str[i -1])))
 				str[i] -= 32;
 			else if (is_upper(str[i]) && is_alfa(str[i -1]))
-				str[i] += 32;*/
+				str[i] += 32;
 		}
 		i++;
 	}
 	return (str);
 }
-int	main(void)
+/*int	main(void)
 {
-	char *str_test = "ola, tudo bem? 42palavras quarenta-e-duas; cinquenta+e+um";
-	#include <stdio.h>
-	printf("string antes: %s", str_test);
+	char str_test[] = "ola, tudo bem? 42palavras quarenta-e-duas; cinquenta+e+um";
+	printf("string antes: %s\n", str_test);
 	ft_strcapitalize(str_test);
-	printf("string depois: %s", str_test);
+	printf("string depois: %s\n", str_test);
 
-}
+}*/
