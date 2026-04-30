6adf1361a951b702149bc7eef32efdb2ff51d1b1
biel8sk <dasaybarreto724@gmail.com>
2026-04-30 17:12:14 -0300

    finalmente consegui terminar o print_memory

--- C02/ex12/ft_print_memory.c
+++ C02/ex12/ft_print_memory.c
@@ -5,52 +5,52 @@ void	write_mem_hex(unsigned long mem_addr, char *base)
 	int	j;
 	char	addr_hex[16];
 	
-	j = 16;
-	while(j > 0)
+	j = 15;
+	while(j >= 0)
 	{
-		addr_hex[j] = base[(mem_addr >> (j * 4)) & 15];
+		addr_hex[j] = base[mem_addr & 15];
+		mem_addr >>= 4;
 		j--;	
 	}
 	write(1, addr_hex, 16);
 	write(1, ": ", 2);
 }
 
-void	write_phrase_hex(unsigned char *start, char *base, unsigned int i)
+void	write_phrase_hex(unsigned char *start, char *base, unsigned int charleft)
 {
 	unsigned int	j;
 
 	j = 0;
-	while(j < 16)
+	while(j < charleft)
 	{
-		if (!start[j + i])
-		{
-			write(1, &base[start[j + i] >> 4], 1);
-			write(1, &base[start[j + i] % 16], 1);
-		}
-		if (!start[j + i + 1])
-		{
-			write(1, &base[start[j + 1 + i] >> 4], 1);
-			write(1, &base[start[j + 1 + i] % 16], 1);
-		}
-		write(1, " ", 1);
-		j += 2;
+		write(1, &base[start[j] >> 4], 1);
+		write(1, &base[start[j] % 16], 1);
+		if (j > 0 && j % 2 == 0)
+			write(1, " ", 1);
+		j++;
 	}
 }
-void	write_phrase_text(unsigned char *start, int limit)
+void	write_phrase_text(unsigned char *start, int charleft)
 {
 	int	j;
 
 	j = 0;
-	while(j < limit)
+	while(j < charleft)
 	{
-		if (*start <= 32 && *start > 126)
-			write(1, ".", 1);
-		else
+		if (start[j] >= 32 && start[j] <= 126)
 			write(1, &start[j], 1);
+		else
+			write(1, ".", 1);
 		j++;
-		start++;
 	}
 	write(1, "\n", 1);
+}
+
+unsigned int	get_space_to_fill(unsigned int charleft)
+{
+	if (charleft < 16)
+		return (charleft);
+	return (16);
 }
 
 void	*ft_print_memory(void *addr, unsigned int size)
@@ -58,23 +58,25 @@ void	*ft_print_memory(void *addr, unsigned int size)
 	unsigned char	*str;
 	unsigned int	i;
 	char		*base;
+	int		charleft;
        
 	base = "0123456789abcdef";
 	i = 0;
+	charleft = 0;
 	str = (unsigned char *) addr;
 	while(i < size)
 	{
+		charleft = get_space_to_fill(size - i);
 		write_mem_hex((unsigned long) &str, base);
-		write_phrase_hex(str, base, i);
-		write_phrase_text(str, );
-		i += 16;
-		str += 16;
+		write_phrase_hex(str, base, charleft);
+		write_phrase_text(str, charleft);
+		i += charleft;
+		str += charleft;
 	}
 	return (addr);
 }
-
 /*int	main(void)
 {
-	char str[] = "a v	ai 123aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-	ft_print_memory(str, 80);
+	char str[] = "Hello\tWorld\n\x01\x02";
+	ft_print_memory(str, 13);
 }*/
